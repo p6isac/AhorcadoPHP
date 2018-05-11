d0a7fc941fa1ff3b25a79ac67b3682a6d8744dcc
Isac Valencia Medina <correodeisac@hotmail.com>
2018-05-10 21:59:08 -0500

    Lo simplifique

--- ahorcado.php
+++ ahorcado.php
@@ -1,83 +1,103 @@
 <?php
 
-echo"<DOCTYPE html>
-	<html lang='es'>
-		<head>
-			<meta charset='UTF-8'>
-			<title>Ahorcado</title>
-		</head>
-		<body>
-			<h1>Probando obtener las palabras</h1>";
-		$arch=fopen('palabras.txt','r');
-		$var=fread($arch,filesize('palabras.txt'));
-		//echo $var;
+session_start();
 
-		$separacion=explode(" ",$var);
-	
-		$random=rand(0, 19);
+if (isset($_SESSION['nom'])){
 
-//palabra random seleccionada del archivo
-		$opcion=$separacion[$random];
-		echo $opcion;
-		echo "<br/>";
-//tamaño de la palabra anterior
-		$tamaño=strlen($opcion);
-		echo ''.$tamaño.'<br/>';
-//desordenamiento de la palabra seleccionada
-		$desordena=str_shuffle($opcion);
-		echo ''.$desordena.'<br/>';
-//nuevo random para escoger localida de la letra de ayuda	
-		$random2=rand(0,$tamaño);
+		echo"<DOCTYPE html>
+			<html lang='es'>
+				<head>
+					<meta charset='UTF-8'>
+					<title>Ahorcado</title>
+				</head>
+				<body>
+					<h1>JUEGO DEL AHORCADO</h1><br/>";
+				echo '<h1>Hola '.$_SESSION['nom'].'</h1>';
 
-		echo ''.$random2.'<br/>';
-//letra de ayuda obtenida con el random anterior y la palabra desordenada		
+				$arch=fopen('palabras.txt','r');
+				$var=fread($arch,filesize('palabras.txt'));
+				//echo $var;
 
-		$layuda=$desordena[$random2-1];
-		echo ''.$layuda.'<br/><br/>';
+				$separacion=explode(" ",$var);
+			
+				$random=rand(0, 19);
 
-
-		for($i=0;$i<$tamaño;$i++){
-
-				
+		//palabra random seleccionada del archivo
+				$opcion=$separacion[$random];
+				$PAL=$opcion;
+				//echo $opcion;
 				echo "<br/>";
-			if($opcion[$i]!=$layuda){
-				$opcion[$i]="___";
+		//tamaño de la palabra anterior
+				$tamaño=strlen($opcion);
+				//echo ''.$tamaño.'<br/>';
+		//desordenamiento de la palabra seleccionada
+				$desordena=str_shuffle($opcion);
+				//echo ''.$desordena.'<br/>';
+		//nuevo random para escoger localida de la letra de ayuda	
+				$random2=rand(0,$tamaño);
+
+				//echo ''.$random2.'<br/>';
+		//letra de ayuda obtenida con el random anterior y la palabra desordenada		
+
+				$layuda=$desordena[$random2-1];
+				//echo ''.$layuda.'<br/><br/>';
+
+
+				for($i=0;$i<$tamaño;$i++){
 				
-				echo $opcion[$i];
-				echo "<br/>";
+						echo "   ";
+					if($opcion[$i]!=$layuda){
+						$opcion[$i]="___";
+						
+						echo $opcion[$i];
+						echo "   ";
+						
+					}elseif ($opcion[$i]=$layuda) {
+
+						$opcion[$i]=$layuda;
+						echo $opcion[$i];			
+					}
+
+				}
+			
 				
-			}elseif ($opcion[$i]=$layuda) {
 
-				$opcion[$i]=$layuda;
-				echo $opcion[$i];			
-			}
+					echo "<br/></br><br/><br/>";
 
-		}
-	
-		fclose($arch);
+					echo"<form method='POST' action='ahorcado2.php'>";
+					echo"<input type='hidden' name='palabra' value='".$PAL."'/>   <br/><br/>";
+					echo"<input type='hidden' name='ayuda' value='".$layuda."'/>   <br/><br/>";
+					
+					echo"					Introduce una letra que creas está entre las faltantes:<br/>";
+					echo"					<input type='text' size='1' maxlength='1' name='letra' />";
+					echo"					<br/><br/>";
+					echo"					<input type='submit' value='Checar'/>";
+					echo"</form>";
 
+					echo	"	
+					<form action='cerrar.php'>
+						<button type='submit' value='Cerrar'>Cerrar</button>
+					</form>	";
 
+					fclose($arch);
+		echo		"</body>
 
-
-echo		"</body>
-	</hmtl>	";	
-
-
-
-
-
-
-
-
-
+			</hmtl>	";	
 
 
 
+		}else{
 
 
+					echo "<script>";
+					echo "alert('Inicia sesion');";  
+					echo "window.location = 'index.php'";
+					echo "</script>";
 
 
+	}
 
+		
 
 
 
